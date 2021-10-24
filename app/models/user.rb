e68634315e77860949ef11a8c058565f742925dc
Cameron Tong <cameron.tong2b@gmail.com>
2021-10-24 13:01:50 -0400

    implement dynamic layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,13 @@ class User < ApplicationRecord
   )
   validates(:password, length: { minimum: 6 })
   has_secure_password
+
+  def self.digest(string)
+    cost = if ActiveModel::SecurePassword.min_cost
+             BCrypt::Engine::MIN_COST
+           else
+             BCrypt::Engine.cost
+           end
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
--- test/integration/user_login_test.rb
+++ test/integration/user_login_test.rb
@@ -13,4 +13,20 @@ class UserLoginTest < ActionDispatch::IntegrationTest
     assert_template "sessions/new"
     assert_not flash.empty?
   end
+
+  test "valid login" do
+    get login_path
+    post login_path,
+         params: {
+           session: {
+             email: "cameron@cameron.com",
+             password: "foobar",
+           },
+         }
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select "a[href=?]", new_list_path
+    assert_select "a[href=?]", lists_path
+    assert_select "a[href=?]", logout_path
+  end
 end
